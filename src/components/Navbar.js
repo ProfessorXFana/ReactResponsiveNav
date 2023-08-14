d58c9bbfd281d839e575b3cf54fe86e37bf4c10c
Professor <sajal.voice@gmail.com>
2023-08-15 03:13:13 +0600

    Update with Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./navbarStyle.css";
-import { TfiYoutube,TfiLinkedin,TfiFacebook } from "react-icons/tfi";
+import mainlogo from "../img/main-logo.png"
 import {GiHamburgerMenu} from "react-icons/gi"
 
 
@@ -9,8 +9,7 @@ function Navbar(){
     return(
         <div className="main-nav">
             <div className="logo">
-                <h2><span>T</span>he
-                <span>O</span>notion</h2>
+                <img src={mainlogo} alt="" />
             </div>
         <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
             <ul>
@@ -20,16 +19,9 @@ function Navbar(){
                 <li><a href="">Contact</a></li>
             </ul>
         </div>
-        <div className="social-media">
-            <ul className="social-media-desktop">
-            <li><a href="#"></a><TfiFacebook className="facebook"/></li>
-            <li><a href="#"></a><TfiLinkedin className="instagram"/></li>
-            <li><a href="#"></a><TfiYoutube className="youtube"/></li>
-            </ul>
             <div className="hamburger-menu">
                 <a href="#" onClick={()=>setShowMediaIcons(!showMediaIcons)}><GiHamburgerMenu/></a>
             </div>
-        </div>
 
         </div>
     ); 
